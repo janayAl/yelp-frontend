043600a1bb90f9764ac4afaaa6071e668349dd40
janayAl <jsallen298705@gmail.com>
2022-09-12 02:17:09 -0500

    log in button and corrected syntax error

--- src/TopNav/TopNav.js
+++ src/TopNav/TopNav.js
@@ -1,16 +1,15 @@
 import React from 'react';
 
 export function TopNav() {
-  return;
+  return (
+    <>
+      <div></div>
 
-  <>
-    <div></div>
-
-    <span>Login</span>
-    <button className="button">Login</button>
-    <div>
-      <span>Sign Up</span>
-      <button className="button">Sign Up</button>
-    </div>
-  </>;
+      <div>
+        <span>Login</span>
+        <button className="button">Login</button>
+      </div>
+      <div></div>
+    </>
+  );
 }
