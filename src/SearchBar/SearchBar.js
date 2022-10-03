53e712454f311a43dad7aece017cb86d61f4677e
janayAl <jsallen298705@gmail.com>
2022-10-03 16:30:21 -0500

    add an extra search area to the searchbar js file

--- src/SearchBar/SearchBar.js
+++ src/SearchBar/SearchBar.js
@@ -4,17 +4,23 @@ import React from "react";
 
 export function SearchBar () {
   return (
-  <>
-<div class="field has-addons">
-  <p class="control">
-    <input class="input" type="text" placeholder="Your Zip Code"/>
-  </p>
-  <p class="control">
-    <button class="button is-static">Search by Zip Code
-    </button>
-  </p>
-</div>
-</>
+    <>
+      <div class="field has-addons">
+        <p class="control">
+          <input class="input" type="text" placeholder="Item" />
+        </p>
+        <p class="control">
+          <button class="button is-static">Search Item</button>
+        </p>
+        <p class="control">
+          <input class="input" type="text" placeholder="Zip Code" />
+        </p>
+
+        <p class="control">
+          <button class="button is-static">Search Zip Code</button>
+        </p>
+      </div>
+    </>
   );
 }
 
