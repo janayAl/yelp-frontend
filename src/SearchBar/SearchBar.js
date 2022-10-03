2d5ed8a50e80e499a79e06291b073171663c082b
janayAl <jsallen298705@gmail.com>
2022-10-03 16:14:38 -0500

    copied searchbar text from bulma

--- src/SearchBar/SearchBar.js
+++ src/SearchBar/SearchBar.js
@@ -2,15 +2,20 @@ import React from "react";
 
 
 
-export function SearchBar({placeholder, data}) {
+export function SearchBar () {
   return (
-<div className='search'>
-  <div className='searchInputs'>
-    <input type='text'/>
-    <dvi className='searchIcon'></dvi>
-  </div>
-  <div className='dataReseult'></div>
+  <>
+<div class="field has-addons">
+  <p class="control">
+    <input class="input" type="text" placeholder="Your Zip Code"/>
+  </p>
+  <p class="control">
+    <a class="button is-static">
+      Search by Zip Code
+    </a>
+  </p>
 </div>
+</>
   );
 }
 
