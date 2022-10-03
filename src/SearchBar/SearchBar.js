3e724923ee3a09bc0a051afaa5d8a9a6223998eb
janayAl <jsallen298705@gmail.com>
2022-10-03 16:36:27 -0500

    change class to className in search bar file for react

--- src/SearchBar/SearchBar.js
+++ src/SearchBar/SearchBar.js
@@ -5,19 +5,19 @@ import React from "react";
 export function SearchBar () {
   return (
     <>
-      <div class="field has-addons">
-        <p class="control">
-          <input class="input" type="text" placeholder="Item" />
+      <div className="field has-addons">
+        <p className="control">
+          <input className="input" type="text" placeholder="Item" />
         </p>
         <p class="control">
-          <button class="button is-static">Search Item</button>
+          <button className="button is-static">Search Item</button>
         </p>
         <p class="control">
-          <input class="input" type="text" placeholder="Zip Code" />
+          <input className="input" type="text" placeholder="Zip Code" />
         </p>
 
-        <p class="control">
-          <button class="button is-static">Search Zip Code</button>
+        <p className="control">
+          <button className="button is-static">Search Zip Code</button>
         </p>
       </div>
     </>
